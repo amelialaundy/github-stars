773c81bb5e10ffe83d13b7a8d882855617ff99fc
Amelia Laundy <amelialaundy@gmail.com>
2019-07-25 19:04:24 +0100

    UI improvements to position components in a grid layout, show icons depending on star ranking

--- src/components/repository.js
+++ src/components/repository.js
@@ -1,13 +1,20 @@
 import React from "react";
+import './Repository.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import moment from "moment";
 
 export class Repository extends React.Component {
   render() {
-    return <div>
-      <p>{this.props.name}</p>
+    return <div className="Repository">
+      <a href={this.props.link}>{this.props.name}</a>
       <p>{this.props.description}</p>
-      <p>{this.props.createdAt}</p>
-      <p>{this.props.stars}</p>
+      <div className="stars">
+      <FontAwesomeIcon
+            icon={this.props.icon} />
+        <p>{this.props.stars}</p>
 
+      </div>
+      <p>Created: {moment(this.props.createdAt).format('Do MMMM YYYY')}</p>
     </div>
   }
 }
--- src/components/repositoryList.js
+++ src/components/repositoryList.js
@@ -1,14 +1,25 @@
 
 import React from 'react';
 import { Repository } from './repository';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStarHalfAlt, faStarHalf, faStar } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faStar);
+library.add(faStarHalfAlt);
+library.add(faStarHalf);
+//<i class="fas fa-star-half-alt"></i>
+const icons = ["star","star-half-alt", "star-half"]
 export class RepositoryList extends React.Component {
   render() {
-    return this.props.repos.map(repo => {
+    return this.props.repos.map((repo, i) => {
       return <Repository
         name={repo.full_name}
         description={repo.description}
         createdAt={repo.created_at}
         stars={repo.stargazers_count}
+        icon={icons[i]}
+        link={repo.html_url}
+        key={repo.id}
       >
       </Repository>
     })
