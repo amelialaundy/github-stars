1577def49d2ff1d27582bfe2281cc296cc3b2a4e
Amelia Laundy <amelialaundy@gmail.com>
2019-07-25 19:03:54 +0100

    actualy search github correctly with :> for dates and language

--- src/services/github.js
+++ src/services/github.js
@@ -1,7 +1,7 @@
-export const getRepositories = async () => {
-  const url = "https://api.github.com/search/repositories?q=language%3Ajavascript&sort=stars";
+export const getRepositories = async (languageName, since) => {
+  const url = `https://api.github.com/search/repositories?q=language:${languageName}+created:>${since}&sort=stars`;
   const response = await fetch(url);
   console.log(response);
   const repositoriesData = await response.json();
-  return repositoriesData.items.slice(0,2);
+  return repositoriesData.items.slice(0,3);
 }
