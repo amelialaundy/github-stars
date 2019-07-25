b2c2963ed4976ca577a0b93e5159bc2eaddd2c99
Amelia Laundy <amelialaundy@gmail.com>
2019-07-25 19:03:05 +0100

    add filtering by last 30 days to app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import logo from '../logo.svg';
 import './App.css';
 import { getRepositories } from '../services/github';
 import { RepositoryList } from './repositoryList';
+import moment from "moment";
 
 export class App extends React.Component {
   languages = ["JavaScript", "Python", "Java", "C++", "Swift", "TypeScript", "Go", "SQL", "HTML", "CSS", "Ruby", "PHP"]
@@ -10,28 +10,30 @@ export class App extends React.Component {
     super(props);
     this.state = {
       loading: false,
-      selectedLanguage: null,
+      selectedLanguage: this.languages[0],
       repositories: []
     };
   }
 
+  getDateThirtyDaysAgo() {
+    return moment().subtract("30", "days").format("YYYY-MM-DD");
+  }
+
   async searchGithub(event) {
     event.preventDefault();
-    const topRepos = await getRepositories(this.state.selectedLanguage);
-    this.setState({repositories: topRepos})
+    this.setState({repositories: []});
+    const topRepos = await getRepositories(this.state.selectedLanguage, this.getDateThirtyDaysAgo());
+    this.setState({repositories: topRepos});
   }
 
   handleLanguageSelect(event) {
-    console.log({event: event.target.value, selected: this.state.selectedLanguage})
-
     this.setState({ selectedLanguage: event.target.value });
   }
 
   render() {
-    return <div className="App">
+    return <div className="App outer-grid">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>Search the top 3 starred GitHub respositories by {this.state.selectedLanguage || "language"}</p>
+        <p>Search the top 3 starred GitHub respositories by {this.state.selectedLanguage || "language"} created in the last 30 days</p>
         <form onSubmit={this.searchGithub.bind(this)}>
           <select
             id="language"
@@ -39,7 +41,7 @@ export class App extends React.Component {
             onChange={this.handleLanguageSelect.bind(this)}
           >
             {this.languages.map(lang => {
-            return <option value={lang}>{lang}</option>
+            return <option value={lang} key={lang}>{lang}</option>
             })}
           </select>
           <input type="submit" value="Search" />
